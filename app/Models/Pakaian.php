cfb29114be241a4567badc25eeba6dc61f7f7a1b
Samuel Sihotang <samsihotang1@gmail.com>
2023-12-15 20:41:04 +0700

    models update

--- app/Models/Pakaian.php
+++ app/Models/Pakaian.php
@@ -7,7 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pakaian extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    protected $guarded = [];
+  protected $guarded = [];
+
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
+
+  public function pesanans()
+  {
+    return $this->belongsTo(Pesanan::class);
+  }
 }
--- app/Models/Pesanan.php
+++ app/Models/Pesanan.php
@@ -7,7 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pesanan extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    protected $guarded = [];
+  protected $guarded = [];
+
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
+
+  public function pakaians()
+  {
+    return $this->hasMany(Pakaian::class);
+  }
 }
